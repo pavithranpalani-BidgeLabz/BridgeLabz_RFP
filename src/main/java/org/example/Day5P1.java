388a78080c330884db1d7421f8d2adec5255e04f
pavithranpalani-BidgeLabz <pavithran.palani@bridgelabz>
2026-03-24 12:05:48 +0530

    Day 5 Problem 1 completed

---
+++ src/main/java/org/example/Day5P1.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+public  class Day5P1{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of fibonacci series: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+
+        }
+
+
+    }
+}
