4803a1c1a24d6d62412a7243999f69b4dede85c3
pavithranpalani-BidgeLabz <pavithran.palani@bridgelabz>
2026-05-16 11:19:32 +0530

    daY16 and 17 completed

---
+++ src/main/java/org/example/FileReadPerformance.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.io.*;
+
+public class FileReadPerformance {
+    private static final String FILE_PATH = "D:/learning/testing.txt";
+
+    public static void testFileReader() throws Exception {
+        long start = System.nanoTime();
+
+        FileReader fr = new FileReader(FILE_PATH);
+        BufferedReader br = new BufferedReader(fr);
+        while (br.read() != -1) {
+        }
+
+        br.close();
+        long end = System.nanoTime();
+        System.out.println("FileReader Time: " + (end - start) / 1_000_000.0 + " ms");
+    }
+
+    public static void testInputStreamReader() throws Exception {
+        long start = System.nanoTime();
+
+        InputStreamReader ir = new InputStreamReader(new BufferedInputStream(new FileInputStream(FILE_PATH)));
+        while (ir.read() != -1) {
+
+        }
+        ir.close();
+        long end = System.nanoTime();
+        System.out.println("InputStreamReader Time: " + (end - start) / 1_000_000.0 + " ms");
+    }
+
+    public static void main(String[] args) {
+        try {
+            testFileReader();
+            testInputStreamReader();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/org/example/SearchSpecificWord.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class SearchSpecificWord {
+    private static String findSearchSpecificWord(String[] strs, String word) {
+        for (String str : strs) {
+            if (str.contains(word)) {
+                return str;
+            }
+        }
+        return "Not found";
+    }
+
+    public static void main(String[] args) {
+        String[] str = {
+                "Java is powerful",
+                "Data Structures are important",
+                "Programming makes problem solving easier"
+        };
+        String word = "makes";
+        String results = findSearchSpecificWord(str, word);
+        System.out.println("Specific Word in sentence: " + results);
+    }
+}
