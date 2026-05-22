7912bbb7ddd8618d9412cb734e08decd508dab45
pavithranpalani-BidgeLabz <pavithran.palani@bridgelabz>
2026-05-22 10:54:23 +0530

    Day26 completed

---
+++ src/main/java/org/example/HotelReservationSystemException.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class HotelReservationSystemException extends Exception {
+    public HotelReservationSystemException(String message) {
+        super(message);
+    }
+}
