4dc69d8fa26db9003daefba458056242a9773959
pavithranpalani-BidgeLabz <pavithran.palani@bridgelabz>
2026-05-27 11:26:55 +0530

    Day 29 completed

---
+++ src/main/java/org/example/CSVStateCensus.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVStateCensus {
+
+    @CsvBindByName(column = "State")
+    public String state;
+
+    @CsvBindByName(column = "Population")
+    public int population;
+
+    @CsvBindByName(column = "AreaInSqKm")
+    public int areaInSqKm;
+
+    @CsvBindByName(column = "DensityPerSqKm")
+    public int densityPerSqKm;
+}
