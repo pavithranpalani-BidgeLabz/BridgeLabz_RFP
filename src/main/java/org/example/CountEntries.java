5ca9ae784b82a93e43c9b050649cde0eede390de
pavithranpalani-BidgeLabz <pavithran.palani@bridgelabz>
2026-05-27 11:20:05 +0530

    Day 27 completed

---
+++ src/main/java/org/example/CountEntries.java
@@ -0,0 +1,12 @@
+package org.example;
+import java.nio.file.*;
+import java.io.IOException;
+
+public class CountEntries {
+    public static void main(String[] args) throws IOException {
+
+        long count = Files.lines(Paths.get("employee.txt")).count();
+
+        System.out.println("Number of Entries: " + count);
+    }
+}
--- src/main/java/org/example/HotelReservationSystemException.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class HotelReservationSystemException extends Exception {
-    public HotelReservationSystemException(String message) {
-        super(message);
-    }
-}
